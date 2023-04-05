83087b2ff6035c820eadee83a455e0af395d236b
yogesh121546 <yogeshkumawats291@gmail.com>
2023-04-05 09:23:08 +0530

    transaction issue fixed

--- node/transaction.js
+++ node/transaction.js
@@ -2,65 +2,26 @@ const express= require('express');
 const transaction= express.Router();
 const {connectDB}= require('./database');
 const { authenticateToken } = require('./jwt');
-
-
+const {insert_transactions, get_user_id}= require('./trans_functions');
 
 transaction.post('/',authenticateToken,(req,res)=>{
     const email = req.user;
-    const {transactions}= req.body;
-    console.log(transactions);
+    let {transactions}= req.body; 
+    let dt = new Date(transactions[0].date);let month = dt.getMonth()+1; let year = dt.getFullYear();
+    insert_transactions(email,month,year,transactions,res);
+});
+
+//get transactions for a past 6 months
+transaction.get('/',authenticateToken,(req,res)=>{
+    const email = req.user;
+    const get_transactions=async()=>{
+        let user_id = await get_user_id(email);
+        var sql = `SEL`
+
+    }
     
- 
-    var sql = `SELECT * FROM users WHERE email="${email}"`;
-    connectDB.query(sql,(err,result)=>{
-        if(err) throw err; 
-        if(result){
-            const temp= result[0].user_id;
-            let user_id= temp;  
-            // console.log("user_id",user_id); 
-            var query ="";
-            let session_trans=[];
-            let dt = new Date(transactions[0].date);
-            let month = dt.getMonth()+1;
-            let year = dt.getFullYear();
-            // console.log(`month:${month}, year:${year} , user_id:${user_id}`)
-            var sqls = `SELECT * from budget_monthly WHERE user_id='${user_id}' AND MONTH(month) = '${month}' AND YEAR(month) = '${year}'`;
-            connectDB.query(sqls,(err,result)=>{  
-               if(err) throw err;
-               if(result){
-                console.log(result[0]);
-                let budget_id= result[0].budget_id;
-                console.log("budget_id",budget_id);
-                console.log(transactions.length);
-                let flag=0;
-                for(let i=0;i<transactions.length;i++){
-                 if(transactions[i].session_id!=null){
-                     
-                     session_trans.push(transactions[i]);
-                 }
-                 else{
-                     if(flag==0){
-                         query += `('${user_id}', '${budget_id}', ${transactions[i].session_id}, '${transactions[i].date}', '${transactions[i].time}', '${transactions[i].amount}', '${transactions[i].category}')`;
-                         flag=1;
-                     }else{
-                         query += `,('${user_id}', '${budget_id}', ${transactions[i].session_id}, '${transactions[i].date}', '${transactions[i].time}', '${transactions[i].amount}', '${transactions[i].category}')`;
-                     }
-                 } 
-                   
-                 }
-                 var sql = `INSERT into transactions(user_id,budget_id,session_id,date,time,amount,category) VALUES ${query}` ;
-                      connectDB.query(sql,(err,result)=>{  
-                         if(err) throw err;
-                         console.log(`transactions are saved in db for user_id: ${user_id}`);   
-                         // let rowsaffected = result.affectedRows();
-                         res.send(`transactions are saved in db for user_id: ${user_id}`); 
-                         }) ;  
-                
-               }
-               
-            })
-        }  
-    }) 
-   
+    
 })
+
+//get sessioin transactions for a particular month only and send the sum to the 
 module.exports= {transaction};
