a5c400a83766b1149ea763146db7d494cc6b3752
unknown <ashmitgupta03@gmail.com>
2023-04-12 11:09:52 +0530

    LS-requests-initiated

--- UI/Login_page/script.js
+++ UI/Login_page/script.js
@@ -1,49 +1,26 @@
-otp_hash = "";
+
 
 let el = document.getElementById("sub");
-var username = document.getElementById("usrnm");
-var password = document.getElementById("pswd");
+
 var user = {};
 
 
 el.addEventListener("click", lgin);
 
-const hashBrowser = val =>
-  crypto.subtle.digest('SHA-256', new TextEncoder('utf-8').encode(val))
-    .then(h => {
-      let hexes = [],
-        view = new DataView(h);
-      for (let i = 0; i < view.byteLength; i += 4)
-        hexes.push(('00000000' + view.getUint32(i).toString(16)).slice(-8));
-      return hexes.join('');
-    });
 
-const validate_otp = async (otp, hsh) => {
-  let b = await hashBrowser(otp);
-  if (b == hsh) {
-    console.log("VALID");
-    return (true);
-  }
-  return false;
-}
 
 function otp() {
   var otpObj = {};
   var otp = document.getElementById("ottp").value;
   user["otp"] = otp;
   console.log(user);
-  if (validate_otp(otp, otp_hash)) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://localhost:3000/register_user", true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(user));
-  }
-  else {
-    console.log("OTP assumed to be incorrect");
-  }
+
 }
 
 function sup() {
+
+  el.removeEventListener("click", lgin);
+  el.addEventListener("click", sgn);
   var div = document.createElement('div');
   div.id = "udiv";
   div.className = "txt_field";
@@ -79,40 +56,31 @@ function lgin() {
   user["email"] = email.value;
   user["password"] = password.value;
 
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", "http://localhost:3000/validate_user_login", true);
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4) {
-      console.log(xhr.response);
-      user_server = xhr.response;
-    }
-  }
 
-  xhr.send(JSON.stringify(user));
 
   console.log(user);
 
 }
 
 function sgn() {
+
+  var users = {};
   var username = document.getElementById("usrnm");
   var password = document.getElementById("pswd");
   var email = document.getElementById("omk");
 
-  user["username"] = username.value;
-  user["password"] = password.value;
-  user["email"] = email.value;
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", "http://localhost:3000/get_otp", true);
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4) {
-      console.log(xhr.response);
-      otp_hash = xhr.response["hash"];
-    }
-  }
-  xhr.send(JSON.stringify(user));
+  users["username"] = username.value;
+  users["password"] = password.value;
+  users["email"] = email.value;
+
+  console.log(username.value);
+  console.log(users);
+
+  axios.post("http//:localhost/signup", users).then(res => {
+    console.log(res);
+  }).catch(err => {
+    console.log(err);
+  });
 
   document.getElementById("udiv").remove();
   document.getElementById("password").remove();
@@ -134,7 +102,6 @@ function sgn() {
   el.removeEventListener("click", sgn);
   el.addEventListener("click", otp);
 
-  console.log(user);
 }
 
 if ("serviceWorker" in navigator) {
