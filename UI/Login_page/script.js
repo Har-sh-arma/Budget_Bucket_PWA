bfc57265f4d2b0202146591ab7f22e60794cf856
Har-sh-arma <harinathsharma303@gmail.com>
2023-04-17 23:15:38 +0530

    dev

--- UI/Login_page/script.js
+++ UI/Login_page/script.js
@@ -148,6 +148,6 @@ function sgn() {
 
 }
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("../../sw.js");
-}
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker.register("../../sw.js");
+// }
