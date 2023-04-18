7d65dc6dc1a398a5c3896628379bccafe98180b3
yogesh121546 <yogeshkumawats291@gmail.com>
2023-04-18 17:54:50 +0530

    profile added

--- node/routes/profile.js
+++ node/routes/profile.js
@@ -8,7 +8,7 @@ const {connectDB}= require('../SQL/database');
 profile.put('/',authenticateToken,(req,res)=>{
     const email= req.user;
     const {name,DOB,location,profile}= req.body;
-   var sql = `UPDATE users SET name='${name}' , DOB='${DOB}' , location='${location}' , profile='${profile} where email='${email}''`
+   var sql = `UPDATE users SET name='${name}' , DOB='${DOB}' , location='${location}' , profile='${profile}' where email='${email}'`
    connectDB.query(sql,(err,result)=>{
     if(err){console.log(err); res.status(400).send("server side err");}
     res.send("updated profile");
